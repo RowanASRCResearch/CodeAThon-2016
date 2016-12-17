f55328c4f3db6fc23457ade925783cbce6b790e5
lapost48 <lapost48@gmail.com>
2016-12-17 03:13:22 -0500

    Successfully interface with BlueNMEA to get GPS data over bluetooth

--- src/GpsTest.java
+++ src/GpsTest.java
@@ -1,7 +1,8 @@
 import jssc.SerialPort;
 import jssc.SerialPortException;
 
-import net.sf.marineapi.nmea.parser.SentenceFactory;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by lapost48 on 12/16/2016.
@@ -10,25 +11,46 @@ public class GpsTest {
 
     public static void main(String[] args) {
 
-        SerialPort gpsPort = new SerialPort("COM3");
-
+        SerialPort gpsPort = new SerialPort("COM4");
+        Pattern p = Pattern.compile("GPGLL,.*A...");
 
         try {
             gpsPort.openPort();
 
             while (true) {
                 byte[] bytes;
+                String[] location = null;
                 if((bytes = gpsPort.readBytes()) != null) {
-                    //SentenceFactory.getInstance().createParser();
-                    System.out.println(bytes);
-//                    for (byte b : bytes)
-//                        System.out.println(b);
+                    String data = new String(bytes);
+                    if(data.contains("GPGLL")) {
+                        Matcher m = p.matcher(data);
+                        m.find();
+                        location = m.group(0).split(",");
+                    }
+                }
+                if(location != null) {
+                    float lat = Float.parseFloat(location[1]);
+                    lat *= (location[2].equals("N")) ? 1 : -1;
+                    float lon = Float.parseFloat(location[3]);
+                    lon *= (location[4].equals("E")) ? 1 : -1;
+
+                    lat = convertToDecimal(lat);
+                    lon = convertToDecimal(lon);
+
+                    System.out.println("(" + lat + ", " + lon + ")");
                 }
             }
         } catch (SerialPortException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static float convertToDecimal(float coord) {
+        int coord_deg = (int) coord / 100;
+        int coord_min = (int) coord % 100;
+        int coord_sec = (int) (coord * 100) % 100;
+        return coord_deg + (coord_min / (float) 60.0) + (coord_sec / (float) 60.0 / (float) 60.0);
     }
 
 }
